4df06cda9bc3c20f6579fe9482c88bab183c7e79
kingofgreatnezzz <kingofgreatnezzz@gmail.com>
2024-10-07 05:16:01 -0700

    back of bb

--- dispatch/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import dispatch_rider_view
-
-urlpatterns = [
-     path('riders/', dispatch_rider_view, name='dispatchriders'),
-
-    # Other URL patterns
-]
