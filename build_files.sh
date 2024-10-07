29c19157b2d8331ade4caf632481073dca77785c
kingofgreatnezzz <kingofgreatnezzz@gmail.com>
2024-10-07 11:55:02 -0700

    another build

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Install pip (if needed)
-if ! command -v pip &> /dev/null
-then
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py --user
-fi
-
 # Install the required dependencies
 pip install -r requirements.txt
 
